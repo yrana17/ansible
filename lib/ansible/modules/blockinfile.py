5a0fa97d1c3dd30818f44b681f168857289374ae
Yogesh Rana <Yogesh.Rana@ibm.com>
2025-03-07 19:52:41 +0530

    handled idempotence for encoding text IO scenario

--- lib/ansible/modules/blockinfile.py
+++ lib/ansible/modules/blockinfile.py
@@ -395,6 +395,12 @@ def main():
     if module._diff:
         diff['after'] = result
 
+    #convert original variable type to bytes
+    if  module.params['encoding']:
+        if original is not None:
+          bytes_object = ''.join(original).encode('utf-8')
+          original=bytes_object
+
     if original == result:
         msg = ''
         changed = False
